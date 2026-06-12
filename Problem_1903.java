30b9ac32fcff2a95129905486580c9be7b720bd6
Jyoti Saini <136901655+JyotiSaini10@users.noreply.github.com>
2026-06-12 23:54:09 +0530

    Add files via upload

---
+++ Problem_1903.java
@@ -0,0 +1,21 @@
+
+public class Problem_1903 {
+
+    public static String largestOddNumber(String num) {
+              for (int i = num.length() - 1; i >= 0; i--) {
+
+            int digit = num.charAt(i) - '0';
+
+            if (digit % 2 != 0) {
+                return num.substring(0, i + 1);
+            }
+        }
+
+        return "";
+    }
+        
+    public static void main(String[] args) {
+        String s= "627";
+        System.out.println(largestOddNumber(s));
+    }
+}
